3588b7b21f14df9159e9f2846f0f8f68c9fe8d41
saidur1529 <saidur.rahman04@northsouth.edu>
2021-04-18 23:36:43 +0600

    burj al arab auth

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCLg_cuoFtUDAjIc8qqnFivsrbzKKPHjr8",
+  authDomain: "burj-al-arab-61f7f.firebaseapp.com",
+  projectId: "burj-al-arab-61f7f",
+  storageBucket: "burj-al-arab-61f7f.appspot.com",
+  messagingSenderId: "181293358700",
+  appId: "1:181293358700:web:6378f02ee48f87e2ce0249",
+  measurementId: "G-1XXG1Q81FE"
+};
+  export default firebaseConfig;
